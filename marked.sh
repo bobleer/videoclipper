55fc5c8a235246c76521b6a623a072ce3596f634
Bob Lee <liwenbo628@gmail.com>
2019-04-30 18:50:20 +0100

    Update marked.sh

--- marked.sh
+++ marked.sh
@@ -6,6 +6,6 @@ echo '将要剪裁的视频拖入Terminal框'
 read videopath
 add='[Marked]'
 
-ffmpeg -i "$videopath" -c:a copy -vf "movie=logo.png [watermark]; [in][watermark] overlay=main_w-overlay_w-10:main_h-overlay_h-10 [out]" "$add${videopath##*/}"
+ffmpeg -i "$videopath" -c:a copy -vf "movie=logo.png [watermark]; [in][watermark] overlay=main_w-overlay_w-5:main_h-overlay_h-5 [out]" "$add${videopath##*/}"
 
 open ./
